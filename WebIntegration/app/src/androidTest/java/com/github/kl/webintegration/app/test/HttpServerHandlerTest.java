9cabdd74088a777047667d886794c39c06095863
Kalle Lindström <lindstrom.kalle@gmail.com>
2014-04-15 10:49:43 +0200

    Added HttpServerHandler test

--- WebIntegration/app/src/androidTest/java/com/github/kl/webintegration/app/test/HttpServerHandlerTest.java
+++ WebIntegration/app/src/androidTest/java/com/github/kl/webintegration/app/test/HttpServerHandlerTest.java
@@ -1,7 +1,68 @@
 package com.github.kl.webintegration.app.test;
 
+import android.content.Context;
+import android.content.Intent;
+
+import com.github.kl.webintegration.app.ForApplication;
+import com.github.kl.webintegration.app.handlers.HttpServerHandler;
+
 import junit.framework.TestCase;
+
+import org.json.JSONObject;
+import org.mockito.ArgumentCaptor;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.ObjectGraph;
+import dagger.Provides;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class HttpServerHandlerTest extends TestCase {
 
+    // SUT
+    @Inject HttpServerHandler httpServerHandler;
+
+    // These objects are injected with @Singleton. Therefore they are they same objects that
+    // are injected in to the class under test, and they can be checked in the test methods.
+    @Inject @ForApplication
+    Context context;
+
+    @Module(
+            injects = HttpServerHandlerTest.class
+    )
+    static class TestModule {
+
+        @Provides @Singleton @ForApplication
+        Context provideApplicationContext() {
+            return mock(Context.class);
+        }
+    }
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        TestHelper.setDexCache();
+
+        ObjectGraph.create(new TestModule()).inject(this);
+    }
+
+    public void testStartsServerServiceWhenOnResult() {
+        httpServerHandler.handleResult(mock(JSONObject.class));
+
+        ArgumentCaptor<Intent> intent = ArgumentCaptor.forClass(Intent.class);
+        verify(context).startService(intent.capture());
+        assertEquals(HttpServerHandler.ServerService.class.getName(),
+                     intent.getValue().getComponent().getClassName());
+    }
+
 }
+
+
+
+
+
+
